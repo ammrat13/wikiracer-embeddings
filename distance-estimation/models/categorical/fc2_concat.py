9158507cc7a840a3c3b8c54d984ecd80c9e9cd47
Ammar Ratnani <ammrat13@gmail.com>
2024-11-14 21:19:32 -0800

    Get rid of dropout
    We're not overfitting yet, so it just slows us down.

--- distance-estimation/models/categorical/fc2_concat.py
+++ distance-estimation/models/categorical/fc2_concat.py
@@ -21,32 +21,19 @@ class CatFC2ConcatModelMetadata(ICategoricalModelMetadata):
             help="Length of the hidden layer",
             default=32,
         )
-        parser.add_argument(
-            "--dropout-rate",
-            type=float,
-            help="Dropout rate for the model",
-            default=0.0,
-        )
 
     def __init__(self, args: argparse.Namespace, class_weights: torch.Tensor):
         super().__init__(args, class_weights)
         self.embedding_length = args.embedding_length
         self.hidden_length = args.hidden_length
-        self.dropout_rate = args.dropout_rate
 
     def get_model(self) -> IModel:
         return CatFC2ConcatModel(
-            self.embedding_length,
-            self.max_distance,
-            self.hidden_length,
-            self.dropout_rate,
+            self.embedding_length, self.max_distance, self.hidden_length
         )
 
     def get_wandb_config(self) -> dict[str, any]:
-        return {
-            "hidden_length": self.hidden_length,
-            "dropout_rate": self.dropout_rate,
-        }
+        return {"hidden_length": self.hidden_length}
 
 
 @torch.compile
@@ -57,18 +44,15 @@ class CatFC2ConcatModel(IModel):
         embedding_length: int,
         max_distance: int,
         hidden_length: int,
-        dropout_rate: float,
     ):
         super().__init__(embedding_length, max_distance)
         self.hidden_linear = torch.nn.Linear(2 * embedding_length, hidden_length)
         self.hidden_values = torch.nn.ReLU()
-        self.hidden_dropout = torch.nn.Dropout(dropout_rate)
         self.output_linear = torch.nn.Linear(hidden_length, max_distance)
 
     def forward(self, s: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         x = torch.cat((s, t), dim=1)
         x = self.hidden_linear(x)
         x = self.hidden_values(x)
-        x = self.hidden_dropout(x)
         x = self.output_linear(x)
         return x
--- distance-estimation/models/categorical/fc3_concat.py
+++ distance-estimation/models/categorical/fc3_concat.py
@@ -27,19 +27,12 @@ class CatFC3ConcatModelMetadata(ICategoricalModelMetadata):
             help="Length of the second hidden layer",
             default=32,
         )
-        parser.add_argument(
-            "--dropout-rate",
-            type=float,
-            help="Dropout rate for the model",
-            default=0.0,
-        )
 
     def __init__(self, args: argparse.Namespace, class_weights: torch.Tensor):
         super().__init__(args, class_weights)
         self.embedding_length = args.embedding_length
         self.hidden_length_1 = args.hidden_length_1
         self.hidden_length_2 = args.hidden_length_2
-        self.dropout_rate = args.dropout_rate
 
     def get_model(self) -> IModel:
         return CatFC3ConcatModel(
@@ -47,14 +40,12 @@ class CatFC3ConcatModelMetadata(ICategoricalModelMetadata):
             self.max_distance,
             self.hidden_length_1,
             self.hidden_length_2,
-            self.dropout_rate,
         )
 
     def get_wandb_config(self) -> dict[str, any]:
         return {
             "hidden_length_1": self.hidden_length_1,
             "hidden_length_2": self.hidden_length_2,
-            "dropout_rate": self.dropout_rate,
         }
 
 
@@ -67,24 +58,19 @@ class CatFC3ConcatModel(IModel):
         max_distance: int,
         hidden_length_1: int,
         hidden_length_2: int,
-        dropout_rate: float,
     ):
         super().__init__(embedding_length, max_distance)
         self.hidden_1_linear = torch.nn.Linear(2 * embedding_length, hidden_length_1)
         self.hidden_1_values = torch.nn.ReLU()
-        self.hidden_1_dropout = torch.nn.Dropout(dropout_rate)
         self.hidden_2_linear = torch.nn.Linear(hidden_length_1, hidden_length_2)
         self.hidden_2_values = torch.nn.ReLU()
-        self.hidden_2_dropout = torch.nn.Dropout(dropout_rate)
         self.output_linear = torch.nn.Linear(hidden_length_2, max_distance)
 
     def forward(self, s: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         x = torch.cat((s, t), dim=1)
         x = self.hidden_1_linear(x)
         x = self.hidden_1_values(x)
-        x = self.hidden_1_dropout(x)
         x = self.hidden_2_linear(x)
         x = self.hidden_2_values(x)
-        x = self.hidden_2_dropout(x)
         x = self.output_linear(x)
         return x
