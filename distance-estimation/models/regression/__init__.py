0334d3a9707f324c278216d0b998c6d1a4ee92ad
Ammar Ratnani <ammrat13@gmail.com>
2024-11-15 02:09:43 -0800

    Ignore low estimates too

--- distance-estimation/models/regression/__init__.py
+++ distance-estimation/models/regression/__init__.py
@@ -28,10 +28,12 @@ class IRegressionModelMetadata(IModelMetadata):
 @torch.compile
 class RegressionModelLoss(torch.nn.Module):
     max_distance: int
+    cutoff: int
 
     def __init__(self, max_distance: int):
         super().__init__()
         self.max_distance = max_distance
+        self.cutoff = max_distance - 1
 
     def forward(
         self,
@@ -39,9 +41,9 @@ class RegressionModelLoss(torch.nn.Module):
         labels: torch.Tensor,
         sample_weights: torch.Tensor,
     ) -> torch.Tensor:
-        targ = torch.where(labels == 0, self.max_distance - 1, labels - 1)
+        targ = torch.where(labels == 0, self.cutoff, labels - 1)
         sqer = (output - targ) ** 2
-        return torch.mean(
-            sample_weights
-            * torch.where((labels == 0) & (output >= self.cutoff), 0.0, sqer)
-        )
+
+        ign_hi = (labels == 0) & (output >= self.cutoff)
+        ign_lo = (labels == 1) & (output <= 0.0)
+        return torch.mean(sample_weights * torch.where(ign_hi | ign_lo, 0.0, sqer))
