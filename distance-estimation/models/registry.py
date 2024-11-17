09d55ee2f87889e5b4c3bcb05826838c7ef2a5e5
Ammar Ratnani <ammrat13@gmail.com>
2024-11-17 02:31:05 -0800

    Cleanup unused classes

--- distance-estimation/models/registry.py
+++ distance-estimation/models/registry.py
@@ -7,22 +7,10 @@ This has to go in a separate file due to circular imports.
 from typing import Type
 
 from models import IModelMetadata
-from models.categorical.fc1_hadamard import CatFC1HadamardModelMetadata
-from models.categorical.fc2_hadamard import CatFC2HadamardModelMetadata
-from models.categorical.fc2_concat import CatFC2ConcatModelMetadata
-from models.categorical.fc3_concat import CatFC3ConcatModelMetadata
-from models.regression.fc2_hadamard import RegFC2HadamardModelMetadata
-from models.regression.fc3_concat import RegFC3ConcatModelMetadata
 from models.regression.fcl_concat import RegFClConcatModelMetadata
 from models.regression.resfcl_concat import RegResFClConcatModelMetadata
 
 MODEL_REGISTRY: dict[str, Type[IModelMetadata]] = {
-    "cat-fc1-hadamard": CatFC1HadamardModelMetadata,
-    "cat-fc2-hadamard": CatFC2HadamardModelMetadata,
-    "cat-fc2-concat": CatFC2ConcatModelMetadata,
-    "cat-fc3-concat": CatFC3ConcatModelMetadata,
-    "reg-fc2-hadamard": RegFC2HadamardModelMetadata,
-    "reg-fc3-concat": RegFC3ConcatModelMetadata,
     "reg-fcl-concat": RegFClConcatModelMetadata,
     "reg-resfcl-concat": RegResFClConcatModelMetadata,
 }
