57db90ba7785386520a23775535d45e5e687a638
Ammar Ratnani <ammrat13@gmail.com>
2024-11-19 00:30:07 -0800

    Fixup some build issues

--- graph-search/src/lib.rs
+++ graph-search/src/lib.rs
@@ -1,3 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use rsmgp_sys::memgraph::Memgraph;
+use rsmgp_sys::mgp::{mgp_graph, mgp_list, mgp_module, mgp_memory, mgp_result};
+use rsmgp_sys::result::MgpResult;
+use rsmgp_sys::rsmgp::{Type, NamedType, set_memgraph_error_msg};
+use rsmgp_sys::{close_module, define_procedure, define_type, init_module};
+
+use c_str_macro::c_str;
+use std::ffi::CString;
+use std::os::raw::c_int;
+use std::panic;
+
+init_module!(|memgraph: &Memgraph| -> MgpResult<()> {
+    memgraph.add_read_procedure(
+        hello_world,
+        c_str!("hello_world"),
+        &[],
+        &[],
+        &[define_type!("output_string", Type::String)],
+    )?;
+    Ok(())
+});
+
+define_procedure!(hello_world, |memgraph: &Memgraph| -> MgpResult<()> {
+    let result = memgraph.result_record()?;
+    result.insert_string(
+        c_str!("output_string"),
+        c_str!("Hello, World!"),
+    )?;
+    Ok(())
+});
+
+close_module!(|| -> MgpResult<()> {
+    Ok(())
+});
